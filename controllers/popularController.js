9d7333a0272395155ebfa9df26f797e4fcf0b576
Andrii Sereda <a.a.sereda@ukr.net>
2023-04-18 14:55:02 +0300

    add popular recipe point without sorting by populate

--- controllers/popularController.js
+++ controllers/popularController.js
@@ -1,5 +1,26 @@
+const { ValidateError, FoundingError } = require("../middleware/errorHandler");
+const { recipieQueryValidete } = require("../schemas/recipieValidate");
+const { popularService } = require("../services/popularService");
+
 const popularController = async (req, res, next) => {
-  res.status(200).json({ message: "ok", code: 200 });
+  const reqValidateParams = recipieQueryValidete.validate(req.params);
+
+  let { page = 1, limit = 4 } = req.query;
+  limit = parseInt(limit);
+  const skip = (parseInt(page) - 1) * limit;
+
+  if (!reqValidateParams.error) {
+    const recipes = await popularService({ skip, limit });
+    if (recipes) {
+      return res.status(200).json({
+        message: "getting popular resipes succes",
+        code: 200,
+        data: recipes,
+        page: page,
+        limit: limit,
+      });
+    } else throw new FoundingError("Recipes not found");
+  } else throw new ValidateError(reqValidateQuery.error);
 };
 
 module.exports = { popularController };
