3d8fc1b24ffc497bb4ea7b59d758c831502a7155
Andrii Sereda <a.a.sereda@ukr.net>
2023-04-25 21:20:30 +0300

    add user info

--- services/userService.js
+++ services/userService.js
@@ -1,7 +1,26 @@
+const Recipe = require("../schemas/recipiesSchema");
 const User = require("../schemas/userSchema");
 
 const getUserDataService = async (_id) => {
   return await User.findOne({ _id }, { password: 0, vCode: 0 });
+};
+
+const getUserInfoService = async (_id) => {
+  const countRecipeAddingToFavorite = await Recipe.find({
+    favorites: {
+      $in: { _id },
+    },
+  }).count();
+
+  const countRecipeAddingToPortal = await await Recipe.find({
+    owner: _id,
+  }).count();
+
+  return {
+    countToFavorite: countRecipeAddingToFavorite,
+    countUserRecipe: countRecipeAddingToPortal,
+    timeInPortal: "",
+  };
 };
 
 const patchUserDataService = async (_id, body, avatarUrl) => {
@@ -23,4 +42,8 @@ const patchUserDataService = async (_id, body, avatarUrl) => {
   }
 };
 
-module.exports = { getUserDataService, patchUserDataService };
+module.exports = {
+  getUserDataService,
+  getUserInfoService,
+  patchUserDataService,
+};
